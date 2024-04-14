da9dd7f5edcde56fe5c001ad76c90ed50bec272e
Igor Joński <jonskiigor@gmail.com>
2024-04-14 18:11:12 +0200

    DW-12: Add app colors, write extensions to parse hex string colors into Flutter ones

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:dw_flutter_app/extensions/hex_string_color_to_color.dart';
+import 'package:flutter/material.dart';
+
+@immutable
+class AppColors {
+  static final loginButtonColor = '#FDFDFD'.toColor();
+  static const loginButtonTextColor = Colors.black;
+  static final backgroundColor = '#181818'.toColor();
+  static final loginAgreementTextColor = '#E7E7E7'.toColor();
+  static final loginAgreementHighlightTextColor = '#FDFDFD'.toColor();
+  const AppColors._();
+}
