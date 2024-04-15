d13c1d6f2f96b9606c2e4b3bf24acfc34b936fbb
Igor Joński <jonskiigor@gmail.com>
2024-04-15 12:42:37 +0200

    DW-12: Create LoginButton to sign in with Google or Apple

--- lib/views/login/login_view.dart
+++ lib/views/login/login_view.dart
@@ -1,4 +1,6 @@
 import 'package:dw_flutter_app/components/divider_with_margins.dart';
+import 'package:dw_flutter_app/constants/strings.dart';
+import 'package:dw_flutter_app/views/login/login_button.dart';
 import 'package:dw_flutter_app/views/login/login_view_terms_agreement_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -8,8 +10,8 @@ class LoginView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Padding(
-      padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 40.0),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 40.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         mainAxisSize: MainAxisSize.max,
@@ -17,11 +19,25 @@ class LoginView extends ConsumerWidget {
           SingleChildScrollView(
             child: Column(
               mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [],
+              children: [
+                LoginButton(
+                  text: Strings.continueWithGoogle,
+                  imagePath: "assets/images/google_logo.png",
+                  onPressed: () {},
+                ),
+                const SizedBox(height: 12.0),
+                LoginButton(
+                  text: Strings.continueWithApple,
+                  imagePath: "assets/images/apple_logo.png",
+                  onPressed: () {},
+                ),
+                const SizedBox(height: 12.0),
+              ],
             ),
           ),
-          Column(
+          const Column(
             children: [
               DividerWithMargins(),
               Padding(
