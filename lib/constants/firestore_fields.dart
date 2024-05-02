cb2e0eef8dd96444c0cf23854d8cfa4c7fd94e05
Igor Joński <jonskiigor@gmail.com>
2024-05-02 23:52:25 +0200

    DW-39: Create necessary Firestore fields

---
+++ lib/constants/firestore_fields.dart
@@ -0,0 +1,49 @@
+class FirestoreEventsFields {
+  static const String title = 'title';
+  static const String description = 'description';
+  static const String type = 'type';
+  static const String room = 'room';
+  static const String timeStart = 'timeStart';
+  static const String timeEnd = 'timeEnd';
+  static const String partner = 'partner';
+  static const String imageSrc = 'imageSrc';
+  FirestoreEventsFields._();
+}
+
+class FirestoreTasksFields {
+  static const String taskId = 'taskId';
+  static const String title = 'title';
+  static const String description = 'description';
+  static const String points = 'points';
+  static const String qrCode = 'qrCode';
+  static const String imageSrc = 'imageSrc';
+  FirestoreTasksFields._();
+}
+
+class FirestoreUsersFields {
+  static const String userId = 'userId';
+  static const String displayName = 'displayName';
+  static const String email = 'email';
+  static const String scannedQrCodes = 'scannedQrCodes';
+  static const String gainedPoints = 'gainedPoints';
+  static const String isWinner = 'isWinner';
+  static const String lastScannedQrCodeTime = 'lastScannedQrCodeTime';
+  FirestoreUsersFields._();
+}
+
+class FirestorePatronsFields {
+  static const String name = 'name';
+  static const String imageSrc = 'imageSrc';
+  FirestorePatronsFields._();
+}
+
+class FirestorePartnersFields {
+  static const String name = 'name';
+  static const String imageSrc = 'imageSrc';
+  static const String package = 'package';
+  FirestorePartnersFields._();
+}
+
+class FirestoreContestFields {
+  static const String endTime = 'endTime';
+}
