84cd834bc5459005e1ed01bf616893d723e21654
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2024-05-12 22:52:06 +0200

    style event page

--- lib/components/calendar/event_card.dart
+++ lib/components/calendar/event_card.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-import '../../constants/app_colors.dart';
-
 class EventCard extends StatelessWidget {
-  EventCard(
-      {super.key,
-      required this.eventTitle,
-      required this.partner,
-      required this.time,
-      required this.room});
+  EventCard({
+    Key? key,
+    required this.eventTitle,
+    required this.partner,
+    required this.time,
+    required this.room,
+  }) : super(key: key);
 
   final String eventTitle;
   final String partner;
@@ -18,45 +17,50 @@ class EventCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Center(
-        child: Card(
-          color: Colors.white10,
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: ListTile(
-              title: Text(
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        vertical: 3.0,
+      ),
+      child: Card(
+        color: Colors.white10,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ListTile(
+            title: Padding(
+              padding: const EdgeInsets.only(right: 30.0),
+              child: Text(
                 eventTitle,
                 style: const TextStyle(
                   fontSize: 16,
                 ),
                 softWrap: true,
               ),
-              subtitle: Text(
-                partner,
-                style: const TextStyle(
-                    fontWeight: FontWeight.normal,
+            ),
+            subtitle: Text(
+              partner,
+              style: const TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 16,
+                color: Color.fromRGBO(255, 255, 255, 0.5),
+              ),
+            ),
+            trailing: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '${time.hour}:${time.minute}',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w800,
                     fontSize: 16,
-                    color: Color.fromRGBO(255, 255, 255, 0.5)),
-              ),
-              trailing: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    '${time.hour}:${time.minute}',
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w800,
-                      fontSize: 16,
-                    ),
                   ),
-                  Text(
-                    room,
-                    style: const TextStyle(
-                      fontSize: 14,
-                    ),
+                ),
+                Text(
+                  room,
+                  style: const TextStyle(
+                    fontSize: 14,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
