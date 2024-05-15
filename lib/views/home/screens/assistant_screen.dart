45f4b77c35ec8cbede7891eef6afea25da31a29f
Gujansko <mateusz.klos150401@gmail.com>
2024-05-15 21:19:27 +0200

    Theme assistant

--- lib/views/home/screens/assistant_screen.dart
+++ lib/views/home/screens/assistant_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dw_flutter_app/views/home/screens/chat_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
@@ -12,6 +13,38 @@ class AssistantScreen extends ConsumerWidget {
 
     return Scaffold(
       body: Chat(
+        theme: DefaultChatTheme(
+          backgroundColor: Theme.of(context).colorScheme.background,
+          inputBackgroundColor: Theme.of(context).colorScheme.secondary,
+          inputTextColor: Theme.of(context).colorScheme.onSecondary,
+          dateDividerTextStyle: TextStyle(
+            color: Theme.of(context).colorScheme.onBackground.withOpacity(0.6),
+            fontSize: 12,
+            fontWeight: FontWeight.w800,
+            height: 1.333,
+          ),
+          emptyChatPlaceholderTextStyle: TextStyle(
+            color: Theme.of(context).colorScheme.onBackground.withOpacity(0.6),
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+            height: 1.5,
+          ),
+          inputSurfaceTintColor: Theme.of(context).colorScheme.secondary,
+          primaryColor: Theme.of(context).colorScheme.primary,
+          secondaryColor: Theme.of(context).colorScheme.primaryContainer,
+          receivedMessageBodyTextStyle: TextStyle(
+            color: Theme.of(context).colorScheme.onPrimaryContainer,
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+            height: 1.5,
+          ),
+          receivedMessageCaptionTextStyle: TextStyle(
+            color: Theme.of(context).colorScheme.onPrimaryContainer,
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
+            height: 1.333,
+          ),
+        ),
         onSendPressed: (partialText) =>
             ref.read(messagesProvider.notifier).handleSendPressed(partialText),
         messages: messages,
