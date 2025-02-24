7baafd2075e98fbc7a10bb1161346c1eac450126
olekgolus11 <olekgolus02@gmail.com>
2025-02-24 21:49:00 +0100

    Delete log

--- lib/provider/images/image_provider.dart
+++ lib/provider/images/image_provider.dart
@@ -4,7 +4,6 @@ Future<List<String>?> getImages(String storagePath) async {
   try {
     final storageRef = FirebaseStorage.instance.ref();
     final imagesRef = storageRef.child(storagePath);
-    print(storagePath);
     final images = await imagesRef.listAll();
     final downloadURLs =
         await Future.wait(images.items.map((e) => e.getDownloadURL()));
